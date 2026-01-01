a4a0fe09c758f0903f49551227e79cdaf6ea50a2
CentralLoaf <hdwater2@gmail.com>
2025-12-31 19:17:52 -0800

    feat: reworks config loading to allow multiple hparam configs

--- src/utils/config.py
+++ src/utils/config.py
@@ -2,16 +2,17 @@ from typing import Any
 import yaml
 
 
-def load_config(config: str) -> dict[str, Any]:
+config: dict[str, Any] | None = None
+
+
+def load_config(filename: str) -> None:
     '''
-    Load a config from src/configs/yaml/
+    Sets the current configuration
     
     Args:
-        config (str): Config name; exclude file extension; assumed to be located in src/configs/yaml/
-        
-    Returns:
-        dict[str, Any]: Loaded configuration
+        filename (str): Config file name; exclude file extension; assumed to be located in src/configs/yaml/
     '''
+    global config
     # Read the config
-    with open(f'src/configs/yaml/{config}.yaml', 'r') as f:
-        return yaml.safe_load(f)
+    with open(f'src/configs/yaml/{filename}.yaml', 'r') as f:
+        config = yaml.safe_load(f)
